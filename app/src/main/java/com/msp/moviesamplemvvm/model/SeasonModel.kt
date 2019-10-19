f8aebf38cd69f35fa25f7f36ae98902358f75cc8
Fatemeh <f.movassaghpour@gmail.com>
2019-10-19 11:56:03 +0330

    add SeasonModel

---
+++ app/src/main/java/com/msp/moviesamplemvvm/model/SeasonModel.kt
@@ -0,0 +1,10 @@
+package com.msp.moviesamplemvvm.model
+
+/**
+ * Created by Fatemeh Movassaghpour on 10/19/2019.
+ */
+class SeasonModel {
+
+    var id: Int? = 0
+    var title: String? = ""
+}
---
+++ app/src/main/java/com/msp/moviesamplemvvm/ui/viewmodel/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.msp.moviesamplemvvm.ui.viewmodel
+
+/**
+ * Created by Fatemeh Movassaghpour on 10/19/2019.
+ */
+class MainViewModel {
+}
