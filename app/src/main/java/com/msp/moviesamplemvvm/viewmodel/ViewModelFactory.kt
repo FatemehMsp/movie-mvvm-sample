094974f4a753a5e5e6af3d9083a7a434ba7e81be
Fatemeh <f.movassaghpour@gmail.com>
2019-11-07 15:05:42 +0330

    add ViewModelFactory

---
+++ app/src/main/java/com/msp/moviesamplemvvm/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,13 @@
+package com.msp.moviesamplemvvm.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+/**
+ * Created by Fatemeh Movassaghpour on 11/7/2019.
+ */
+class ViewModelFactory(private val param: String) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return ViewModelFactory(param) as T
+    }
+}
