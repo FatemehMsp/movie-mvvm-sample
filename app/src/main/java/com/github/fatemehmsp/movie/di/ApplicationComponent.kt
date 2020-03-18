d64deff681397f65e110eb4ce5ea2caa69186e02
Fatemeh <f.movassaghpour@gmail.com>
2020-03-18 11:23:50 +0330

    add ApplicationComponent

---
+++ app/src/main/java/com/github/fatemehmsp/movie/di/ApplicationComponent.kt
@@ -0,0 +1,26 @@
+package com.github.fatemehmsp.movie.di
+
+import androidx.databinding.Bindable
+import com.github.fatemehmsp.movie.App
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+/**
+ * Created by Fatemeh Movassaghpour on 3/18/2020.
+ */
+@Singleton
+@Component
+interface ApplicationComponent {
+
+    fun inject(app:App)
+
+    @Component.Builder
+    interface Builder{
+
+       @BindsInstance
+        fun application(app: App) : Builder
+
+        fun Build() : ApplicationComponent
+    }
+}
